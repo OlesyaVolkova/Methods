c424809297b757e4da85f745555e6e5b5e2aeeb4
Olesya <olesya@work>
2023-09-18 22:41:31 +0400

    Functions

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,14 @@ class Main {
         System.out.print("Введите два числа: ");
         int a = kb.nextInt();
         int b = kb.nextInt();
-
         int c = Math.Sum(a, b);
+
         System.out.println("%d + %d = %d".formatted(a, b, c));
         System.out.println("%d - %d = %d".formatted(a, b, Math.Diff(a, b)));
         System.out.println("%d * %d = %d".formatted(a, b, Math.Prod(a, b)));
         System.out.println("%d / %d = %f".formatted(a, b, Math.Quot(a, b)));
+        System.out.println("Факториал числа %d -> %d".formatted(a, Math.Fact(a)));
+        System.out.println("%d в %d степени = %d".formatted(a, b, Math.Power(a, b)));
     }
 }
 
@@ -36,4 +38,22 @@ class Math {
         //Quotient - частное
         return (double) a / b;
     }
+
+    static int Fact(int a) {
+        //Factorial
+        int fact = 1;
+        for (int x = 1; x <= a; x++) {
+            fact *= x;
+        }
+        return fact;
+    }
+
+    static int Power(int a, int b) {
+        int result = 1;
+        for (int i = 0; i < b; i++) {
+            result *= a;
+        }
+        return result;
+    }
 }
+
