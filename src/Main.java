6235fb1ff4c6edd0e84ea1cee9541c54b3623baf
“Lesya” <olesya@home>
2023-09-21 21:21:24 +0400

    Generics DONE

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,91 @@
+import java.util.Random;
 import java.util.Scanner;
 
-class Main {
+public class Main<T> {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        System.out.println("Введите два числа: ");
-        int a = kb.nextInt();
-        int b = kb.nextInt();
+        System.out.print("Введите размер массива: ");
+        int n = kb.nextInt();
+        Integer[] arr = new Integer[n];
 
-        int c = Sum(a, b);
-        System.out.println("%d + %d = %d".formatted(a, b, c));
-        System.out.println("%d - %d = %d".formatted(a, b, Diff(a, b)));
-        System.out.println("%d * %d = %d".formatted(a, b, Prod(a, b)));
-        System.out.println("%d / %d = %d".formatted(a, b, Quot(a, b)));
+        FillRand(arr);
+        Print(arr);
+  //      Sort(arr);
+        Print(arr);
+
+        Double[] brr = new Double[n];
+        FillRand(brr, 0, 100);
+        Print(brr);
+
+        Character[] c_arr = new Character[] {'H', 'e', 'l', 'l', 'o'};
+        Print(c_arr);
     }
-    static int Sum(int a, int b)
+    static void FillRand(Integer[] arr)
     {
-        //Sum - сумма
-        int c = a + b;
-        return c;
+        Random rand = new Random(0);
+        for(int i = 0; i < arr.length; i++)
+        {
+            arr[i] = rand.nextInt(100);
+        }
+    }
+    static void FillRand(Integer[] arr, int minRand, int maxRand)
+    {
+        Random rand = new Random(0);
+        for(int i = 0; i < arr.length; i++)
+        {
+            arr[i] = rand.nextInt(minRand, maxRand);
+        }
+    }
+    static void FillRand(Double[] arr)
+    {
+        Random rand = new Random(0);
+        for(int i = 0; i < arr.length; i++)
+        {
+            arr[i] = rand.nextDouble(100);
+        }
+    }
+    static void FillRand(Double[] arr, int minRand, int maxRand)
+    {
+        Random rand = new Random(0);
+        for(int i = 0; i < arr.length; i++)
+        {
+            arr[i] = rand.nextDouble(minRand, maxRand);
+        }
     }
 
-    static int Diff(int a, int b)
+  /* static<T>  T Sum(T[] arr)
     {
-        //Difference - разность
-        return a - b;
+        T sum T();
+        for (int i=0; i< arr.length; i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+*/
+
+    static<T> void Print(T[] arr)
+    {
+        for(int i= 0; i < arr.length; i++)
+        {
+            System.out.print(arr[i] + "\t");
+        }
+        System.out.println();
     }
 
-    static int Prod(int a, int b)
+  /*  static<T> void Sort(T[] arr)
     {
-        //Product - произведение
-        return a * b;
-    }
-
-    static double Quot(int a, int b)
-    {
-        //Quotient - частное
-        return (double)a / b;
-    }
+        for (int i=0; i <arr.length; i++)
+        {
+            for (int j = i+1; j < arr.length; j++)
+            {
+                if(arr[j] < arr[i])
+                {
+                    T buffer = arr[i];
+                    arr[i]=arr[j];
+                    arr[j] = buffer;
+                }
+            }
+        }
+    }*/
 }
+
